33841b1416d24c808ab8d0f95a60f99e1a909e55
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-02-10 00:56:33 +0100

    Add bStats Metrics

--- src/main/java/fr/kikoplugins/kikoapi/KikoAPI.java
+++ src/main/java/fr/kikoplugins/kikoapi/KikoAPI.java
@@ -1,12 +1,16 @@
 package fr.kikoplugins.kikoapi;
 
 import fr.kikoplugins.kikoapi.updatechecker.UpdateChecker;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KikoAPI extends JavaPlugin {
     private static final String MODRINTH_PROJECT_ID = "nwOXHH0K";
+    private static final int BSTATS_PLUGIN_ID = 29448;
 
     private static KikoAPI instance;
+
+    private Metrics bStats;
 
     @Override
     public void onLoad() {
@@ -17,13 +21,15 @@ public class KikoAPI extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
 
+        this.bStats = new Metrics(this, BSTATS_PLUGIN_ID);
+
         if (this.getConfig().getBoolean("update-checker.enabled", true))
             new UpdateChecker(this, MODRINTH_PROJECT_ID);
     }
 
     @Override
     public void onDisable() {
-
+        this.bStats.shutdown();
     }
 
     public static KikoAPI getInstance() {
