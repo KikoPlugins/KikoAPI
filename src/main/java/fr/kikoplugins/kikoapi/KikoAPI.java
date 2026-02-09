c566cba6aa016f94db876c6f10c7f46effa83d8d
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-02-10 00:42:45 +0100

    Add Update Checker

--- src/main/java/fr/kikoplugins/kikoapi/KikoAPI.java
+++ src/main/java/fr/kikoplugins/kikoapi/KikoAPI.java
@@ -1,15 +1,30 @@
 package fr.kikoplugins.kikoapi;
 
+import fr.kikoplugins.kikoapi.updatechecker.UpdateChecker;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KikoAPI extends JavaPlugin {
+    private static KikoAPI instance;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
 
+        if (this.getConfig().getBoolean("update-checker.enabled", true))
+            new UpdateChecker(this, "nwOXHH0K");
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static KikoAPI getInstance() {
+        return instance;
     }
 }
