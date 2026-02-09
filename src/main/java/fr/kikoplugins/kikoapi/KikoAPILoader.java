b3889fb950f58ec7eb44f61de73733126c316be6
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-02-09 23:46:52 +0100

    Add HikariCP as lib

---
+++ src/main/java/fr/kikoplugins/kikoapi/KikoAPILoader.java
@@ -0,0 +1,24 @@
+package fr.kikoplugins.kikoapi;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.repository.RemoteRepository;
+
+@SuppressWarnings("UnstableApiUsage")
+public class KikoAPILoader implements PluginLoader {
+    @Override
+    public void classloader(PluginClasspathBuilder classpathBuilder) {
+        MavenLibraryResolver resolver = new MavenLibraryResolver();
+
+        try {
+            resolver.addRepository(new RemoteRepository.Builder("central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
+        } catch (NoSuchMethodError err) {
+            resolver.addRepository(new RemoteRepository.Builder("central", "default", "https://maven-central.storage-download.googleapis.com/maven2").build());
+        }
+
+        resolver.addDependency(new Dependency(new DefaultArtifact("com.zaxxer:HikariCP:7.0.2"), null));
+    }
+}
