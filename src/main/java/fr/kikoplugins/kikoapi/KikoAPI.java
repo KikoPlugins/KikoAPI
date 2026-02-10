075497411c9441f8385ab398a12747e9d09c0693
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-02-10 13:57:47 +0100

    Revert "Add Update Checker (#2)"
    This reverts commit 4c4c720db1cd7943531022d9d3280e84f913f198.
    
    # Conflicts:
    #	src/main/java/fr/kikoplugins/kikoapi/KikoAPI.java

--- src/main/java/fr/kikoplugins/kikoapi/KikoAPI.java
+++ src/main/java/fr/kikoplugins/kikoapi/KikoAPI.java
@@ -1,34 +1,17 @@
 package fr.kikoplugins.kikoapi;
 
-import fr.kikoplugins.kikoapi.updatechecker.UpdateChecker;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KikoAPI extends JavaPlugin {
-    private static final String MODRINTH_PROJECT_ID = "nwOXHH0K";
-
-    private static KikoAPI instance;
-    
-    @Override
-    public void onLoad() {
-        instance = this;
-    }
-
     @Override
     public void onEnable() {
-        saveDefaultConfig();
 
-        if (this.getConfig().getBoolean("update-checker.enabled", true))
-            new UpdateChecker(this, MODRINTH_PROJECT_ID);
     }
 
     @Override
     public void onDisable() {
 
-    }
-
-    public static KikoAPI getInstance() {
-        return instance;
     }
 
     public static boolean isUnitTest() {
