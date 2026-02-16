0ebbec3b3541f5bf1051624c4609a5badde2cc69
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2026-02-16 05:39:22 +0100

    Fixed dupes & fixed losing items, added comments the code is weird to understand

--- src/main/java/fr/kikoplugins/kikoapi/menu/listeners/MenuListener.java
+++ src/main/java/fr/kikoplugins/kikoapi/menu/listeners/MenuListener.java
@@ -2,33 +2,61 @@ package fr.kikoplugins.kikoapi.menu.listeners;
 
 import fr.kikoplugins.kikoapi.menu.Menu;
 import fr.kikoplugins.kikoapi.menu.event.KikoInventoryClickEvent;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
+import java.util.EnumSet;
+
 public class MenuListener implements Listener {
+    // These actions could lead to duping so that's a no no
+    private static final EnumSet<InventoryAction> DISALLOWED_ACTIONS = EnumSet.of(
+            InventoryAction.COLLECT_TO_CURSOR,
+            InventoryAction.MOVE_TO_OTHER_INVENTORY
+    );
+
     @EventHandler(ignoreCancelled = true)
     public void onInventoryClick(InventoryClickEvent event) {
         Inventory inventory = event.getClickedInventory();
-        if (inventory == null || event.getCurrentItem() == null)
+        if (inventory == null)
             return;
 
-        Player player = (Player) event.getWhoClicked();
-
-        InventoryHolder topHolder = player.getOpenInventory().getTopInventory().getHolder(false);
-        if (topHolder instanceof Menu)
+        // Remove the ability for players to shift-click items in the menus
+        // While being able to move their items in their inventory
+        // getInventory will always be the top inventory
+        InventoryHolder topHolder = event.getInventory().getHolder(false);
+        if (topHolder instanceof Menu && DISALLOWED_ACTIONS.contains(event.getAction()))
             event.setCancelled(true);
 
+        // Check if a player click on a menu
         InventoryHolder holder = inventory.getHolder(false);
         if (!(holder instanceof Menu menu))
             return;
 
         event.setCancelled(true);
 
+        // Check if the player click on an item in the menu
+        // If the player click on an item that means it's a component
+        if (event.getCurrentItem() == null)
+            return;
+
         KikoInventoryClickEvent clickEvent = new KikoInventoryClickEvent(event, menu.getContext());
         menu.handleClick(clickEvent);
+    }
+
+    // That disables all the way of dragging items when you have a menu open
+    // I haven't found a simple & efficient way to block only dragging inside the menu
+    @EventHandler(ignoreCancelled = true)
+    public void onInventoryDrag(InventoryDragEvent event) {
+        Inventory inventory = event.getInventory();
+        InventoryHolder holder = inventory.getHolder(false);
+        if (!(holder instanceof Menu))
+            return;
+
+        event.setCancelled(true);
     }
 }
